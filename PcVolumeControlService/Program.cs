db655cadda68d18569b68f57b9df00a6c2e80dc4
Matthew Bolger <xan1000@gmail.com>
2020-07-16 14:34:38 +1000

    Removed file logging for non-production.

--- PcVolumeControlService/Program.cs
+++ PcVolumeControlService/Program.cs
@@ -8,13 +8,18 @@ namespace PcVolumeControlService
     {
         public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
-        private static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args).
-            ConfigureLogging((context, logging) => logging.AddFile(context.Configuration.GetSection("Logging"))).
-            ConfigureServices(services =>
-            {
-                services.AddSingleton<IClient, Client>();
-                services.AddSingleton<CachingCoreAudioController>();
-                services.AddHostedService<Worker>();
-            }).UseWindowsService();
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).
+                ConfigureLogging((context, logging) =>
+                {
+                    if(context.HostingEnvironment.IsProduction())
+                        logging.AddFile(context.Configuration.GetSection("Logging"));
+                }).
+                ConfigureServices(services =>
+                {
+                    services.AddSingleton<IClient, Client>();
+                    services.AddSingleton<CachingCoreAudioController>();
+                    services.AddHostedService<Worker>();
+                }).UseWindowsService();
     }
 }
